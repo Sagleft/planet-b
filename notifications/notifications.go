1c94d4f19238795ac6982dfc408eed4d5f7f9dbf
Sagleft <sagleft@inbox.ru>
2021-04-25 02:46:52 +0300

    add notification base

--- notifications/notifications.go
+++ notifications/notifications.go
@@ -1,1 +1,24 @@
 package notifications
+
+// GodNotificator - simulation alert gizmo
+type GodNotificator struct {
+	MessengerService Messenger
+}
+
+// Messenger for notifications
+type Messenger struct {
+	Name     string
+	BotToken string
+	ChatID   string
+}
+
+func newGodNotificator() *GodNotificator {
+	noty := GodNotificator{
+		MessengerService: Messenger{
+			Name:     "Telegram",
+			BotToken: "",
+			ChatID:   "",
+		},
+	}
+	return &noty
+}
